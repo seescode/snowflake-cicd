1d142b321470e3c917fa100e88e28233b1ae44d0
seescode <yongc.programmer@gmail.com>
2026-05-11 10:30:17 -1000

    Fix

--- snowflake/tables/members.sql
+++ snowflake/tables/members.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS TABLE members (
+CREATE TABLE IF NOT EXISTS members (
     member_id       VARCHAR(36)     NOT NULL,
     first_name      VARCHAR(100)    NOT NULL,
     last_name       VARCHAR(100)    NOT NULL,
